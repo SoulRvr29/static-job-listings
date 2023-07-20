a09d307fa348b239db1011898d56ffaf14552b90
SoulRvr <pawelc29@interia.pl>
2023-07-20 17:21:28 +0200

    working filters adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,78 @@
-import data from "./data.json";
+import dataJson from "./data.json";
 import Job from "./Job";
+import { useState } from "react";
+
+// role
+// level
+// languages
+// tools
 
 function App() {
+  const [actualData, changeActualData] = useState(dataJson);
+  const [filterItems, changeFilterItems] = useState([]);
+  // changeActualData();
+
+  const clearFilter = () => {
+    changeFilterItems((items) => (items = []));
+    changeActualData((data) => (data = dataJson));
+  };
+
+  const removeFilter = (check) => {
+    changeFilterItems((arr) => arr.filter((item) => item != check));
+    // let returnData = dataJson;
+    // returnData = returnData.filter((obj) => {
+    //   return (
+    //     !obj.role == check &&
+    //     !obj.level == check &&
+    //     !obj.languages.includes(check) &&
+    //     !obj.tools.includes(check)
+    //   );
+    // });
+    // console.log(returnData);
+    // changeActualData((data) => (data = [...data, ...returnData]));
+  };
+
+  const addFilter = (check) => {
+    !filterItems.includes(check) &&
+      changeFilterItems((arr) => (arr = [...arr, check]));
+    changeActualData((data) =>
+      data.filter((obj) => {
+        return (
+          obj.role == check ||
+          obj.level == check ||
+          obj.languages.includes(check) ||
+          obj.tools.includes(check)
+        );
+      })
+    );
+  };
+
   return (
     <>
       <div className="bkg"></div>
-      <div className="filter">
-        <span className="filter-item">Frontend</span>
-        <span className="filter-item">CSS</span>
-        <span className="filter-item">JavaScript</span>
+      <div
+        className="filter"
+        // style={
+        //   filterItems.length > 0 ? { display: "flex" } : { display: "none" }
+        // }
+      >
+        <div className="filter-items-container">
+          {filterItems.map((item) => (
+            <div
+              className="filter-item"
+              onClick={() => removeFilter(item)}
+              key={filterItems.indexOf(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div className="clear" onClick={clearFilter}>
+          Clear
+        </div>
       </div>
-      {data.map((item) => (
-        <Job
-          company={item.company}
-          position={item.position}
-          isNew={item.new}
-          featured={item.featured}
-          postedAt={item.postedAt}
-          contract={item.contract}
-          location={item.location}
-          languages={item.languages}
-          logo={item.logo}
-        />
+      {actualData.map((item) => (
+        <Job data={item} addFilter={addFilter} key={item.id} />
       ))}
     </>
   );
--- src/Job.jsx
+++ src/Job.jsx
@@ -1,37 +1,38 @@
-const Job = ({
-  company,
-  position,
-  isNew,
-  featured,
-  postedAt,
-  contract,
-  location,
-  languages,
-  logo,
-}) => {
+const Job = ({ addFilter, data }) => {
   return (
-    <section className={isNew ? "left-border" : ""}>
+    <section className={data.new ? "left-border" : ""}>
       <div className="logo">
-        <img src={logo} alt="logo" />
+        <img src={data.logo} alt="logo" />
       </div>
       <div className="data">
-        <ul>
-          <li>
-            <span className="company">{company}</span>{" "}
-            {isNew ? <span className="new">NEW!</span> : ""}
-            {featured ? <span className="featured">FEATURED</span> : ""}
-          </li>
-          <li className="position">{position}</li>
-          <li>
-            <span className="third-line">{postedAt}</span>{" "}
-            <span className="third-line">{contract}</span>{" "}
-            <span className="third-line">{location}</span>
-          </li>
-        </ul>
+        <div className="first-line-container">
+          <span className="company">{data.company}</span>{" "}
+          {data.new ? <span className="new">NEW!</span> : ""}
+          {data.featured ? <span className="featured">FEATURED</span> : ""}
+        </div>
+        <div className="second-line-container">{data.position}</div>
+        <div className="third-line-container">
+          <span className="third-line">{data.postedAt}</span>{" "}
+          <span className="third-line">{data.contract}</span>{" "}
+          <span className="third-line">{data.location}</span>
+        </div>
       </div>
-      <div className="languages">
-        {languages.map((lang) => (
-          <div className="lang">{lang}</div>
+      <div className="itemsList">
+        <div className="item" onClick={() => addFilter(data.role)}>
+          {data.role}
+        </div>
+        <div className="item" onClick={() => addFilter(data.level)}>
+          {data.level}
+        </div>
+        {data.languages.map((lang) => (
+          <div key={lang} className="item" onClick={() => addFilter(lang)}>
+            {lang}
+          </div>
+        ))}
+        {data.tools.map((tool) => (
+          <div key={tool} className="item" onClick={() => addFilter(tool)}>
+            {tool}
+          </div>
         ))}
       </div>
     </section>
